c19f6bac17745972f96e1d7f3e1c58c656048c6b
ggzzggzz <ggzz553@gmail.com>
2022-03-04 12:26:24 +0900

    0304 middle

---
+++ src/chap05/lecture/C07PrimitiveType.java
@@ -0,0 +1,13 @@
+package chap05.lecture;
+
+public class C07PrimitiveType {
+	public static void main(String[] args) {
+		int i1 = 3;
+		method1(i1);
+		System.out.println(i1);
+	}
+	
+	public static void method1(int i) {
+		i = 300;
+	}
+}
