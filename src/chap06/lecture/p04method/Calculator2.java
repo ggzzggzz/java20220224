0bc500985fc66c3e2f830e3ad68b647f6a4078d8
ggzzggzz <ggzz553@gmail.com>
2022-03-11 16:47:14 +0900

    0311

---
+++ src/chap06/lecture/p04method/Calculator2.java
@@ -0,0 +1,31 @@
+package chap06.lecture.p04method;
+
+public class Calculator2 {
+	
+	// varargs (가변길이 매개변수)
+	int sum(int... nums) {
+		
+		System.out.println("nums의 길이: " + nums.length);
+		
+		int sum = 0;
+		
+		for(int num : nums) {
+			sum += num;
+		}
+		
+		return sum;
+	}
+	
+	// 가변길이 매개변수는 항상 마지막에 작성
+	int varSum(String name, int... nums) {
+		return 0;
+	}
+	
+	void concat(String... names) {
+		String sum = "";
+		for (String name : names) {
+			sum += name;
+		}
+		System.out.println(sum);
+	}
+}
