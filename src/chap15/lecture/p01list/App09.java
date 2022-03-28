e9ba443f1fd777e1f34fdd605a98d54189655ce2
ggzzggzz <71123177+ggzzggzz@users.noreply.github.com>
2022-03-28 16:55:49 +0900

    0328

---
+++ src/chap15/lecture/p01list/App09.java
@@ -0,0 +1,20 @@
+package chap15.lecture.p01list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App09 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < 10; i++) {
+			list.add(i);
+		}
+		System.out.println(list);
+		
+		// 홀수 element 삭제
+		list.removeIf(e -> e % 2 != 0);
+		
+		System.out.println(list); // [0, 2, 4, 6, 8]
+	}
+}
