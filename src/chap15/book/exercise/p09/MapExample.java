90d179e2dbbcc2823b9a0ae7564457c118df1658
ggzzggzz <71123177+ggzzggzz@users.noreply.github.com>
2022-03-30 19:11:03 +0900

    0330

---
+++ src/chap15/book/exercise/p09/MapExample.java
@@ -0,0 +1,36 @@
+package chap15.book.exercise.p09;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapExample {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("blue", 96);
+		map.put("hong", 86);
+		map.put("white", 92);
+		
+		String name = null;
+		int maxScore = 0;
+		int totalScore = 0;
+		
+		// 작성 위치
+		Set<String> keySet = map.keySet();
+		for(String key : keySet) {
+			if(maxScore < map.get(key)) {
+				name = key;
+				maxScore = map.get(key);
+			}
+			totalScore += map.get(key);
+		}
+		System.out.println("평균점수: " + (totalScore / map.size()));
+		System.out.println("최고점수: " + maxScore);
+		System.out.println("최고점수를 받은 아이디: " + name);
+	}
+}
+
+
+
+
+
